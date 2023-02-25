6661df1ad49c166fe8146bdc68181056260cc247
Prastowo Widayanto <106642078+BoPras@users.noreply.github.com>
2023-02-25 20:57:15 +0700

    Add files via upload

---
+++ SQLquery_google_related_views.sql
@@ -0,0 +1,15 @@
+# Exploring-the-Popularity-of-Wikipedia-Articles-with-BigQuery-and-SQL
+# Google-Related-View
+SELECT
+  language,
+  title,
+  SUM(views) AS views
+FROM
+  `bigquery-samples.wikipedia_benchmark.Wiki10B`
+WHERE
+  title LIKE '%Google%'
+GROUP BY
+  language,
+  title
+ORDER BY
+  views DESC;
